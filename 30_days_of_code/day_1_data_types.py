6509d864c5937757067afdc804a1e47c2ab7557f
deannawarren <mrs.deanna.warren@gmail.com>
2016-04-28 19:42:23 -0700

    30 Days of Code - Day 1

---
+++ 30_days_of_code/day_1_data_types.py
@@ -0,0 +1,22 @@
+#https://www.hackerrank.com/challenges/30-data-types
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables. (THIS IS NOT NEEDED IN PYTHON DUE TO DYNAMIC TYPING)
+
+# Read and save an integer, double, and String to your variables. (DOUBLES ARE FLOATS IN PYTHON)
+integer = int(raw_input())
+float = float(raw_input())
+string = raw_input()
+
+# Print the sum of both integer variables on a new line.
+print i + integer
+
+# Print the sum of the double variables on a new line.
+print d + float
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + string
